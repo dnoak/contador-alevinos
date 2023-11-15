91aa8b31041fa084d84d47ebb73ae3f3339f2154
dnoak <daniel.ortega.carvalho@gmail.com>
2023-11-15 19:48:14 -0400

    antigo

--- src/models/yolov8/train_yolov8.py
+++ src/models/yolov8/train_yolov8.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 from datetime import datetime
 
-project_path = r'D:/Documentos/Projetos/tcc/contagem-ovos-larvas-peixe/'
+project_path = r'.'
 
 def train(model):
     alevino_model = YOLO(model)
-    train_data = r'data\YOLO_cropped_dataset\train_test_val_split\data.yaml'
+    train_data = r'F:\TCC\contagem-larvas\data\datasets\yolo\data.yaml'
     time = datetime.now().strftime(r'%Y-%m-%d_%H-%M-%S')
     print(time)
     results = alevino_model.train(
@@ -20,7 +20,7 @@ def train(model):
     )
 
 def train_all_models():
-    models = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt', 'yolov8x.pt']
+    models = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt']#, 'yolov8x.pt']
     for model in models:
         try: train(model)
         except: print(f'Error on model {model}')
